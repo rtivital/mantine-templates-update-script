13ffd63a66c4b576380252a4d06382db0ffcbcab
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-28 13:50:55 +0400

    Setup git cleaning before comming changes to template

--- src/commit-changes.ts
+++ src/commit-changes.ts
@@ -1,6 +1,8 @@
 import { execa } from "execa";
 
 export async function commitChanges(templatePath: string, version: string) {
+  await execa("git", ["clean", "-df"], { cwd: templatePath });
+  await execa("git", ["checkout", "--", "."], { cwd: templatePath });
   await execa("git", ["add", "."], { cwd: templatePath });
   await execa(
     "git",

0792106b6661ad048471c80b98443985fa2161d8
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-25 15:34:03 +0400

    Add checkout to master branch

--- src/utils/clean-template.ts
+++ src/utils/clean-template.ts
@@ -7,6 +7,7 @@ export async function cleanTemplate(template: Template) {
   try {
     await execa("git", ["clean", "-df"], { cwd: template.rootPath });
     await execa("git", ["checkout", "--", "."], { cwd: template.rootPath });
+    await execa("git", ["checkout", "master"], { cwd: template.rootPath });
     signale.info(`Template ${chalk.cyan(template.name)} has been cleaned`);
   } catch (error) {
     signale.error(`Failed to clean ${chalk.red(template.name)} template`);
